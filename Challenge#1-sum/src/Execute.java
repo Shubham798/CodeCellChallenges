d8fe26d3799c839bc8d722f7025ef878d6572a66
Shubham798 <42290145+Shubham798@users.noreply.github.com>
2019-08-02 16:54:26 +0530

    Add files via upload

---
+++ Challenge#1-sum/src/Execute.java
@@ -0,0 +1,6 @@
+public class Execute {
+    public static void main(String[] args) {
+        Sum s1= new Sum();
+        System.out.println("The answer is "+(int)(s1.sum(10)+s1.sum(100)+s1.sum(200)));
+    }
+}
---
+++ Challenge#1-sum/src/Sum.java
@@ -0,0 +1,9 @@
+public class Sum {
+    double sum(double n){
+        int s=0;
+        for(int i=1;i<=n;i++){
+            s+=i;
+        }
+        return s;
+    }
+}

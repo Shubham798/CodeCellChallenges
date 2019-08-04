0d1eccfa9bf67a05d1a2c99e26a305e407274869
Shubham798 <42290145+Shubham798@users.noreply.github.com>
2019-08-04 12:16:44 +0530

    Add files via upload

--- Challenge#1.3-cube/src/Cube.java
+++ Challenge#1.3-cube/src/Cube.java
@@ -1,10 +1,17 @@
 public class Cube {
-    double cube(double n){
-        double sum=0;
-        for (double i=1;i<=n;i++) {
-            double x = Math.pow(i, 3);
-            sum+=x;
-        }
-        return sum;
+    long cube(long n){
+        long sum=0;
+
+            long x= n/3;
+            long y= n%3;
+
+            sum+=(x*18);
+            if(y==1)
+                sum+=1;
+            else if (y==2)
+                sum+=9;
+
+            return sum;
     }
+
 }
--- Challenge#1.3-cube/src/Execute.java
+++ Challenge#1.3-cube/src/Execute.java
@@ -1,10 +1,10 @@
 public class Execute {
     public static void main(String[] args) {
         Cube c1= new Cube();
-        double a=c1.cube(5);
-        double b=c1.cube(500);
-        double c=c1.cube(5000000000l);
-        double answer=(a*b*c);
+        long a=c1.cube(5);
+        long b=c1.cube(500);
+        long c=c1.cube(5000000000l);
+        long answer=(a*b*c);
         answer%=1000007;
         System.out.println("The answer is "+answer);
     }
